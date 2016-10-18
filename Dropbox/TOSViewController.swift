3b062519ac3b18d1c1929c8917e4391b1c4e876a
chrisarg0 <cargonish22@gmail.com>
2016-10-17 23:56:22 -0700

    Final Commit

--- Dropbox/TOSViewController.swift
+++ Dropbox/TOSViewController.swift
@@ -12,7 +12,7 @@ class TOSViewController: UIViewController {
     
     @IBOutlet weak var tosWebView: UIWebView!
     
-    let url = "https://www.dropbox.com/"
+    let url = "https://www.dropbox.com/terms?mobile=1"
     
     override func viewDidLoad() {
         super.viewDidLoad()
